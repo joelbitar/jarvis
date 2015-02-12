6815e3a46627d04a817f0b5a4907d8496bc58ec2
Joel Shapiro <joel@joelbitar.se>
2015-02-12 20:40:39 +0100

    Basic rest calls

--- node/device/views.py
+++ node/device/views.py
@@ -1,5 +1,8 @@
 from device.models import Device
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+from rest_framework import renderers
+from rest_framework.response import Response
 from device.serializers import DeviceSerializer
 
 
--- node/node/urls.py
+++ node/node/urls.py
@@ -4,8 +4,13 @@ from rest_framework import routers
 
 from device.views import DeviceViewSet
 
+device_detail = DeviceViewSet.as_view({
+    'get': 'retrieve',
+})
+
 router = routers.DefaultRouter()
 router.register(r'devices', DeviceViewSet)
+router.register(r'devices/(?P<pk>[0-9]+)/$', device_detail, base_name='devices')
 
 
 urlpatterns = [
