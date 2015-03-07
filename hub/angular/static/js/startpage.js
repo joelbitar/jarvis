1feba073b4ca0711f4fa574c0a1c1319bd0f368a
Joel Shapiro <joel@joelbitar.se>
2015-03-07 16:34:59 +0100

    material

--- hub/angular/static/js/startpage.js
+++ hub/angular/static/js/startpage.js
@@ -1,7 +1,6 @@
 /**
  *
  * Created by joel on 2015-03-02.
- */
 
 var jarvis_startpage = angular.module('jarvis.startpage', ['ngRoute', 'restangular'])
 .config(['$routeProvider', function($routeProvider){
@@ -17,3 +16,22 @@ var jarvis_startpage = angular.module('jarvis.startpage', ['ngRoute', 'restangul
             console.log(devices);
         });
 }]);
+
+ */
+
+// var jarvis_startpage = angular.module('jarvis.startpage', ['ngRoute']);
+var jarvis_startpage = angular.module('jarvis.startpage', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider){
+        $routeProvider.when(
+            '/', {
+                templateUrl: template_url('startpage.html')
+            }
+        )
+}])
+.controller('StartpageDeviceController', ['$scope', 'Restangular',  function($scope, Restangular) {
+        Restangular.all(api_url('devices')).getList().then(function(devices){
+            $scope.devices = devices;
+            console.log(devices);
+        });
+}]);
+
