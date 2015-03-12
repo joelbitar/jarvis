6bf85402821d589157f819cc6b7600af901749ae
Joel Shapiro <joel@joelbitar.se>
2015-03-12 22:10:49 +0100

    Device groups in UI

--- hub/angular/static/js/startpage.js
+++ hub/angular/static/js/startpage.js
@@ -7,19 +7,25 @@ var jarvis_startpage = angular.module('jarvis.startpage', ['ngRoute'])
             }
         )
 }])
-.controller('StartpageDeviceController', ['$scope', 'Restangular',  function($scope, Restangular) {
-        Restangular.all('devices').getList().then(function(devices){
-            $scope.devices = devices;
+.controller('StartpageDeviceController', ['$scope', '$rootScope', 'Restangular',  function($scope, $rootScope, Restangular) {
+        $scope.$on('refresh-devices', function(){
+            Restangular.all('devices').getList().then(function(devices){
+                $scope.devices = devices;
+            });
         });
+        $scope.$broadcast('refresh-devices');
 }])
 .controller('StartpageSensorController', ['$scope', 'Restangular',  function($scope, Restangular) {
         Restangular.all('sensors').getList().then(function(sensors){
             $scope.sensors = sensors;
         });
-}]).controller('StartpageDeviceGroupController', ['$scope', 'Restangular',  function($scope, Restangular) {
-        Restangular.all('groups').getList().then(function(groups){
-            $scope.groups = groups;
+}]).controller('StartpageDeviceGroupController', ['$scope', '$rootScope', 'Restangular',  function($scope, $rootScope, Restangular) {
+        $scope.$on('refresh-groups', function() {
+            Restangular.all('groups').getList().then(function (groups) {
+                $scope.groups = groups;
+            });
         });
+        $scope.$broadcast('refresh-groups');
 }]);
 
 
