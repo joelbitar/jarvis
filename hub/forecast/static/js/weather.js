f79a0fce0a8cefccd25fe163c64b8e42514732ad
Joel Shapiro <joel@joelbitar.se>
2015-03-27 07:15:58 +0100

    Weater module does not override startpage module

--- hub/forecast/static/js/weather.js
+++ hub/forecast/static/js/weather.js
@@ -1,19 +1,19 @@
 /**
  *
  * Created by joel on 2015-03-25.
- */
+*/
 
-var jarvis_startpage = angular.module('jarvis.startpage', ['ngRoute'])
+var jarvis_weather = angular.module('jarvis.weather', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider){
         $routeProvider.when(
             '/weather/', {
                 templateUrl: template_url('weather.html')
             }
         )
-}])
-.controller('WeatherController', ['$scope', 'Restangular', function($scope, Restangular){
+}]).controller('WeatherController', ['$scope', 'Restangular', function($scope, Restangular){
     Restangular.all('forecasts').getList().then(function(forecasts){
         console.log(forecasts);
         $scope.forecasts = forecasts;
     });
 }]);
+
