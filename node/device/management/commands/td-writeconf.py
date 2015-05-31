2996677a97e910fa3d993896e3eefea99782dc9a
Joel Shapiro <joel@joelbitar.se>
2015-05-31 20:54:01 +0200

    Acutally write the config

--- node/device/management/commands/td-writeconf.py
+++ node/device/management/commands/td-writeconf.py
@@ -3,11 +3,20 @@ __author__ = 'joel'
 from django.core.management.base import BaseCommand, CommandError
 
 from device.conf import RestartTelldusDaemon
+from device.conf import TellstickConfigWriter
+
+from device.models import Device
 
 class Command(BaseCommand):
     help = 'Restarts daemon'
 
     def handle(self, *args, **options):
+        tellstick_config_writer = TellstickConfigWriter(
+            Device.objects.all()
+        )
+        print('Writing config...')
+        tellstick_config_writer.write_config()
+
         restart_telldus_daemon = RestartTelldusDaemon()
         restart_telldus_daemon.restart()
 
