4662525ad6ecd9cbfef21dcc9dd4443a64c06a94
Joel Shapiro <joel.shapiro@redpill-linpro.com>
2016-12-31 10:11:39 +0100

    remove serviceworker state thingy

--- hub/progressive/static/bootstrap.js
+++ hub/progressive/static/bootstrap.js
@@ -7,23 +7,6 @@ if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/service_worker.js', {scope: '/'}).then(function(registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-
-            var serviceWorker;
-            if (registration.installing) {
-                serviceWorker = registration.installing;
-            } else if (registration.waiting) {
-                serviceWorker = registration.waiting;
-            } else if (registration.active) {
-                serviceWorker = registration.active;
-            }
-
-            if (serviceWorker) {
-                console.log("ServiceWorker phase:", serviceWorker.state);
-
-                serviceWorker.addEventListener('statechange', function (e) {
-                    console.log("ServiceWorker phase:", e.target.state);
-                });
-            }
         }).catch(function(err) {
             // registration failed :(
             console.log('ServiceWorker registration failed: ', err);
