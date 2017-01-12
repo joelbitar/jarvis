beec7bfd7e29ef642b78901367a04dbf86c10469
Joel Shapiro <joel.shapiro@redpill-linpro.com>
2017-01-12 07:42:02 +0100

    No CSRF checks in githook view on hub

--- hub/git_hook/urls.py
+++ hub/git_hook/urls.py
@@ -1,9 +1,10 @@
 __author__ = 'joel'
 
 from django.conf.urls import patterns, url
+from django.views.decorators.csrf import csrf_exempt
 
 from git_hook.views import GitHookView
 
 urlpatterns = patterns('',
-    url(r'^hook/$', GitHookView.as_view(), name='git_hook'),
+    url(r'^hook/$', csrf_exempt(GitHookView.as_view()) , name='git_hook'),
 )
