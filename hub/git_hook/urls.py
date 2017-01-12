e7646b15bdb9b633aea3e070d38c409b099b9336
Joel Shapiro <joel.shapiro@redpill-linpro.com>
2017-01-12 07:44:25 +0100

    Just remove a space in hook urls

--- hub/git_hook/urls.py
+++ hub/git_hook/urls.py
@@ -6,5 +6,5 @@ from django.views.decorators.csrf import csrf_exempt
 from git_hook.views import GitHookView
 
 urlpatterns = patterns('',
-    url(r'^hook/$', csrf_exempt(GitHookView.as_view()) , name='git_hook'),
+    url(r'^hook/$', csrf_exempt(GitHookView.as_view()), name='git_hook'),
 )
